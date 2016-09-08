732288bb29b57812e78c7515ddfba9cf6d031381
mollocon44 <oconnormollyc@gmail.com>
2016-09-08 11:54:31 -0700

    Update plotCuboct.py

--- plotCuboct.py
+++ plotCuboct.py
@@ -1,4 +1,4 @@
-
+# plots one cuboct as a 3d line graph. Not sure how to integrate this with buildRectangles code for visualizations
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
